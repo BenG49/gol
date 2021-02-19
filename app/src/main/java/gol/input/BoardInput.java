9c6a6a1faa3b280548f667c3a745b4ec16d0745d
BenG49 <ben@goldfisher.com>
2021-02-18 22:25:52 -0500

    Add key repeat presses

--- app/src/main/java/gol/input/BoardInput.java
+++ app/src/main/java/gol/input/BoardInput.java
@@ -1,6 +1,7 @@
 package gol.input;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 import gol.game.Board;
 import gol.game.Vector2;
@@ -8,13 +9,23 @@ import gol.game.Vector2;
 public class BoardInput {
     private Board b;
     private KeyBinding keyBind;
-    private HashMap<String, Integer> keyCooldownTimer;
+    private HashSet<String> lastKeyPressed;
+    private HashMap<String, Integer> keyRepeat;
 
-    private final int KEY_COOLDOWN = 100;
+    // multiplier for zooming, higher is faster zooming
     private final double ZOOM_MULT = 1.5;
+    // multiplier for movementSpeed
     private final double MOVEMENT_MULT = 0.1;
+    // slowest step time
     private final int MAX_STEP_TIME = 750;
+    // interval to increase stepTime by
     private final int STEP_TIME_INTERVAL = (int)(MAX_STEP_TIME/10);
+    
+    // <--THESE VALUES ARE COMPLETELY DEPENDENT ON PROGRAM SPEED-->
+    // amount of time to wait until spamming
+    private final int KEY_REPEAT_CUTOFF = 500;
+    // interval to wait between keys to activate when spamming
+    private final int KEY_REPEAT_MODULO = 40;
 
     private int stepTimeMillis;
     private boolean stepAuto;
@@ -36,112 +47,82 @@ public class BoardInput {
         run = true;
         stepAuto = false;
         stepTimeMillis = STEP_TIME_INTERVAL*5;
-        
-        keyCooldownTimer = new HashMap<String, Integer>();
 
-        hashPutInit(keyBind.toggleAutoKey());
-        hashPutInit(keyBind.singleStepKey());
-        hashPutInit(keyBind.quitKey());
-        hashPutInit(keyBind.zoomOut());
-        hashPutInit(keyBind.zoomIn());
-        hashPutInit(keyBind.up());
-        hashPutInit(keyBind.down());
-        hashPutInit(keyBind.left());
-        hashPutInit(keyBind.right());
-        hashPutInit(keyBind.speedUp());
-        hashPutInit(keyBind.speedDown());
-        hashPutInit(keyBind.toOrigin());
-        hashPutInit(keyBind.clear());
+        lastKeyPressed = new HashSet<String>();
+        keyRepeat = new HashMap<String, Integer>();
     }
 
     public void checkKeys() {
+
         movementSpeed = (b.WIDTH/cellScreenLen)*MOVEMENT_MULT;
 
-        for (HashMap.Entry<String, Integer> mapElement : keyCooldownTimer.entrySet()) {
-            if (mapElement.getValue() > 0)
-                keyCooldownTimer.replace(mapElement.getKey(), mapElement.getValue()-1);
-        }
-
-
         // KEYS
-        if (keyCanBePressed(keyBind.toggleAutoKey())) {
+        if (keyCanBePressed(keyBind.toggleAutoKey()))
             stepAuto = !stepAuto;
-            startTimer(keyBind.toggleAutoKey());
-        }
         
-        if (!stepAuto && keyCanBePressed(keyBind.singleStepKey())) {
+        if (!stepAuto && keyCanBePressed(keyBind.singleStepKey()))
             b.step();
-            startTimer(keyBind.singleStepKey());
-        }
 
-        if (keyCanBePressed(keyBind.quitKey())) {
-            // run = false;
-            startTimer(keyBind.quitKey());
-        }
+        if (keyCanBePressed(keyBind.quitKey()))
+            ;// run = false; // COMMENTED FOR NOW BECAUSE IT'S ANNOYING
 
-        if (keyCanBePressed(keyBind.zoomOut())) {
+        if (keyCanBePressed(keyBind.zoomOut()))
             zoom(false);
-            startTimer(keyBind.zoomOut());
-        }
 
-        if (keyCanBePressed(keyBind.zoomIn())) {
+        if (keyCanBePressed(keyBind.zoomIn()))
             zoom(true);
-            startTimer(keyBind.zoomIn());
-        }
 
-        if (keyCanBePressed(keyBind.up())) {
+        if (keyCanBePressed(keyBind.up()))
             screenPos = screenPos.add(new Vector2(0, -movementSpeed));
-            startTimer(keyBind.up());
-        }
 
-        if (keyCanBePressed(keyBind.down())) {
+        if (keyCanBePressed(keyBind.down()))
             screenPos = screenPos.add(new Vector2(0, movementSpeed));
-            startTimer(keyBind.down());
-        }
 
-        if (keyCanBePressed(keyBind.left())) {
+        if (keyCanBePressed(keyBind.left()))
             screenPos = screenPos.add(new Vector2(-movementSpeed, 0));
-            startTimer(keyBind.left());
-        }
 
-        if (keyCanBePressed(keyBind.right())) {
+        if (keyCanBePressed(keyBind.right()))
             screenPos = screenPos.add(new Vector2(movementSpeed, 0));
-            startTimer(keyBind.right());
-        }
 
-        if (keyCanBePressed(keyBind.speedUp())) {
-            if (stepTimeMillis > 2)
-                stepTimeMillis = Math.max(stepTimeMillis - STEP_TIME_INTERVAL, 2);
-            startTimer(keyBind.speedUp());
-        }
+        if (keyCanBePressed(keyBind.speedUp()) && stepTimeMillis > 2)
+            stepTimeMillis = Math.max(stepTimeMillis - STEP_TIME_INTERVAL, 2);
 
-        if (keyCanBePressed(keyBind.speedDown())) {
-            if (stepTimeMillis < MAX_STEP_TIME)
-                stepTimeMillis += STEP_TIME_INTERVAL;
-            startTimer(keyBind.speedDown());
-        }
+        if (keyCanBePressed(keyBind.speedDown()) && stepTimeMillis < MAX_STEP_TIME)
+            stepTimeMillis += STEP_TIME_INTERVAL;
 
-        if (keyCanBePressed(keyBind.toOrigin())) {
+        if (keyCanBePressed(keyBind.toOrigin()))
             screenPos = new Vector2(-b.WIDTH/cellScreenLen/2, -b.HEIGHT/cellScreenLen/2);
-            startTimer(keyBind.toOrigin());
-        }
 
-        if (keyCanBePressed(keyBind.clear())) {
+        if (keyCanBePressed(keyBind.clear()))
             b.clearCells();
-            startTimer(keyBind.toOrigin());
-        }
     }
 
     private boolean keyCanBePressed(String key) {
-        return keyCooldownTimer.get(key) == 0 && b.hasKey(key);
-    }
+        // key held down
+        if (b.hasKey(key)) {
+            if (!keyRepeat.containsKey(key))
+                keyRepeat.put(key, 0);
+            
+            Integer temp = keyRepeat.get(key);
+            keyRepeat.replace(key, ++temp);
+            
+            if (temp >= KEY_REPEAT_CUTOFF && temp % KEY_REPEAT_MODULO == 0)
+                return true;
+        }
 
-    private void hashPutInit(String key) {
-        keyCooldownTimer.put(key, 0);
-    }
+        // key initial press
+        if (!lastKeyPressed.contains(key) && b.hasKey(key)) {
+            lastKeyPressed.add(key);
+            return true;
+        }
 
-    private void startTimer(String key) {
-        keyCooldownTimer.replace(key, KEY_COOLDOWN);
+        // key release
+        if (lastKeyPressed.contains(key) && !b.hasKey(key)) {
+            lastKeyPressed.remove(key);
+            keyRepeat.remove(key);
+        }
+
+        return false;
     }
 
     private void zoom(boolean zoomIn) {
